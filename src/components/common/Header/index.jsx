b17a5783c2dccdd64cad6990b3e4176005b7307b
Danny MB <danielmasias4@gmail.com>
2024-03-13 18:33:02 -0400

    I modified hero module adding coverflow transition and set 5 imagenes for a dinning room, living room and weeding room

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   const { user } = useAuth(); // Assuming you have an authentication context or hook
 
   return (
-    <header className="fixed flex top-0 left-0 w-full h-14 bg-white-500 p-1 border-b-2">
+    <header className="fixed flex top-0 left-0 w-full h-14 bg-white p-1 border-b-2">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-black text-2xl font-bold">Furnish Fantasy</h1>
         <nav className="text-xl font-semibold flex space-x-4">
@@ -12,7 +12,7 @@ const Header = () => {
             <li className="text-black">Dashboard</li>
            
           </ul>
-          <div className="text-black">
+          <div  className="text-black">
             {user ? (
               <>
                 <span className="mr-2">Welcome, {user?.name}</span>
