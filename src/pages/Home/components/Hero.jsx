252101cc5a164d72e8154f5d8136da61b2c8d212
Danny MB <danielmasias4@gmail.com>
2024-03-13 18:25:49 -0400

    I modified hero module adding coverflow transition set 5 imagenes for a dinning room, living room and weeding rom

--- src/pages/Home/components/Hero.jsx
+++ src/pages/Home/components/Hero.jsx
@@ -9,12 +9,6 @@ import {
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/autoplay";
 const slides = [
   { image: "https://via.placeholder.com/600x400?text=Slide+1" },
   { image: "https://via.placeholder.com/600x400?text=Slide+2" },
