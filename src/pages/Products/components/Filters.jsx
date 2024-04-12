67ec593deebf0f3dd6dd4a69906e5772bd1ca127
git-nitin01 <94100392+git-nitin01@users.noreply.github.com>
2024-04-12 08:08:43 -0400

    fixed reset filters

--- src/pages/Products/components/Filters.jsx
+++ src/pages/Products/components/Filters.jsx
@@ -42,11 +42,21 @@ const Filters = ({ categories, categoryNames, filterState, setFilterState }) => 
                         className="block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
                         onChange={(e) => setFilterState({ ...filterState, price: e.target.value })}
                     >
-                        <option value="">All Prices</option>
-                        <option value="0-25">$0 - $25</option>
-                        <option value="25-50">$25 - $50</option>
-                        <option value="50-100">$50 - $100</option>
-                        <option value="100+">$100+</option>
+                        {
+                            filterState.price === "" ? <option value="" selected>All Prices</option> : <option value="">All Prices</option>
+                        }
+                        {
+                            filterState.price === "0-25" ? <option value="0-25" selected>$0 - $25</option> : <option value="0-25">$0 - $25</option>
+                        }
+                        {
+                            filterState.price === "25-50" ? <option value="25-50" selected>$25 - $50</option> : <option value="25-50">$25 - $50</option>
+                        }
+                        {
+                            filterState.price === "50-100" ? <option value="50-100" selected>$50 - $100</option> : <option value="50-100">$50 - $100</option>
+                        }
+                        {
+                            filterState.price === "100+" ? <option value="100+" selected>$100+</option> : <option value="100+">$100+</option>
+                        }
                     </select>
                 </div>
                 <div>
@@ -56,11 +66,21 @@ const Filters = ({ categories, categoryNames, filterState, setFilterState }) => 
                         className="block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
                         onChange={(e) => setFilterState({ ...filterState, sort: e.target.value })}
                     >
-                        <option value="">Default</option>
-                        <option value="price-asc">Price: Low to High</option>
-                        <option value="price-desc">Price: High to Low</option>
-                        <option value="name-asc">Name: A to Z</option>
-                        <option value="name-desc">Name: Z to A</option>
+                        {
+                            filterState.sort === "" ? <option value="" selected>Default</option> : <option value="">Default</option>
+                        }
+                        {
+                            filterState.sort === "price-asc" ? <option value="price-asc" selected>Price - Low to High</option> : <option value="price-asc">Price - Low to High</option>
+                        }
+                        {
+                            filterState.sort === "price-desc" ? <option value="price-desc" selected>Price - High to Low</option> : <option value="price-desc">Price - High to Low</option>
+                        }
+                        {
+                            filterState.sort === "name-asc" ? <option value="name-asc" selected>Name - A to Z</option> : <option value="name-asc">Name - A to Z</option>
+                        }
+                        {
+                            filterState.sort === "name-desc" ? <option value="name-desc" selected>Name - Z to A</option> : <option value="name-desc">Name - Z to A</option>
+                        }
                     </select>
                 </div>
             </div>
